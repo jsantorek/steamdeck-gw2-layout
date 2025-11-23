9332e9d079e64906f5139ed15198a44cd2fca97a
Jakub Santorek <jakub.santorek@gmail.com>
2025-11-23 22:53:41 +0100

    theme and assets changes

--- _includes/script.js
+++
@@ -1,9 +0,0 @@
-function toggleDarkMode(el) {
-    var theme = 'light'
-    if (el.innerText == '☪') {
-        el.innerText = '☀'; theme = 'dark';
-    } else {
-        el.innerText = '☪';
-    }
-    document.documentElement.setAttribute('data-theme', theme)
-}
